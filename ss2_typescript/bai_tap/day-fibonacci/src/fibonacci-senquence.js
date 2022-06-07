ec44850fa5e88839e5d066628d5066f9304488e8
Hoang Long <longhoang6668@gmail.com>
2022-06-07 22:20:55 +0700

    fix comment

--- ss2_typescript/bai_tap/day-fibonacci/src/fibonacci-senquence.js
+++ ss2_typescript/bai_tap/day-fibonacci/src/fibonacci-senquence.js
@@ -1,13 +1,4 @@
-// let num: number = 20;
-// let nextNum: number;
-// let num1: number = 0;
-// let num2: number = 1;
-// for (let i = 0; i < num; i++) {
-//     console.log(num1);
-//     nextNum = num1 + num2;
-//     num1 = num2;
-//     num2 = nextNum;
-// }
+
 function totalNumberOfFibonacci(maximumCount) {
     var number1 = 0;
     var number2 = 1;
