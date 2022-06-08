27a6d5f574fa2f0ad1ea11eaee8a89305745aad0
Hoang Long <longhoang6668@gmail.com>
2022-06-08 09:36:33 +0700

    fix comment

--- ss2_typescript/bai_tap/day-fibonacci/src/fibonacci-senquence.js
+++ ss2_typescript/bai_tap/day-fibonacci/src/fibonacci-senquence.js
@@ -1,18 +1,21 @@
-
+var number1 = 0;
+var number2 = 1;
+var nextNumber;
+var sum = 0;
 function totalNumberOfFibonacci(maximumCount) {
-    var number1 = 0;
-    var number2 = 1;
-    var nextNumber;
-    var sum = 0;
-    for (var i = 0; i < maximumCount; i++) {
+    var size = maximumCount;
+    if (size > 0) {
         console.log("the fibonacci sequence is: " + number1);
+        sum += number1;
         nextNumber = number1 + number2;
         number1 = number2;
         number2 = nextNumber;
-        sum += nextNumber;
+        size = size - 1;
+        totalNumberOfFibonacci(size);
     }
-    return sum;
+    else {
+        console.log("total is: " + sum);
+    }
 }
-
-var result = totalNumberOfFibonacci(20);
-console.log("the total is: " + result);
+totalNumberOfFibonacci(20);
+// console.log("the total is: " + result);
--- ss2_typescript/bai_tap/day-fibonacci/src/fibonacci-senquence.ts
+++ ss2_typescript/bai_tap/day-fibonacci/src/fibonacci-senquence.ts
@@ -1,18 +1,21 @@
-
+let number1: number = 0;
+let number2: number = 1;
+let nextNumber: number;
+let sum: number = 0;
 function totalNumberOfFibonacci(maximumCount: number) {
-    let number1: number = 0;
-    let number2: number = 1;
-    let nextNumber: number;
-    let sum: number = 0;
-    for (let i = 0; i < maximumCount; i++) {
+    let size = maximumCount;
+    if (size > 0) {
         console.log("the fibonacci sequence is: " + number1);
+        sum += number1;
         nextNumber = number1 + number2;
         number1 = number2;
         number2 = nextNumber;
-        sum += nextNumber;
-    }
-    return sum;
+            size = size - 1;
+            totalNumberOfFibonacci(size);
+    }else {
+            console.log("total is: "+sum);
+        }
 }
 
-let result: number = totalNumberOfFibonacci(20);
-console.log("the total is: " + result);
+ totalNumberOfFibonacci(20);
+// console.log("the total is: " + result);
