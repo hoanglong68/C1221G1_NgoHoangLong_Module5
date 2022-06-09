63415092cb47cc68851fbae232643d0e17cb2fe3
Hoang Long <longhoang6668@gmail.com>
2022-06-09 08:40:19 +0700

    fix comment

--- ss2_typescript/bai_tap/day-fibonacci/src/fibonacci-senquence.js
+++ ss2_typescript/bai_tap/day-fibonacci/src/fibonacci-senquence.js
@@ -2,20 +2,27 @@ var number1 = 0;
 var number2 = 1;
 var nextNumber;
 var sum = 0;
+var arrFibonacci = [];
 function totalNumberOfFibonacci(maximumCount) {
     var size = maximumCount;
     if (size > 0) {
-        console.log("the fibonacci sequence is: " + number1);
-        sum += number1;
+        arrFibonacci.push(number1);
         nextNumber = number1 + number2;
         number1 = number2;
         number2 = nextNumber;
         size = size - 1;
         totalNumberOfFibonacci(size);
     }
-    else {
-        console.log("total is: " + sum);
-    }
+    return arrFibonacci;
 }
-totalNumberOfFibonacci(20);
-// console.log("the total is: " + result);
+function sumFibonacciSequence(array) {
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
+;
+arrFibonacci = totalNumberOfFibonacci(20);
+console.log(arrFibonacci);
+sum = sumFibonacciSequence(arrFibonacci);
+console.log(sum);
--- ss2_typescript/bai_tap/day-fibonacci/src/fibonacci-senquence.ts
+++ ss2_typescript/bai_tap/day-fibonacci/src/fibonacci-senquence.ts
@@ -2,19 +2,28 @@ let number1: number = 0;
 let number2: number = 1;
 let nextNumber: number;
 let sum: number = 0;
+let arrFibonacci: number[] = [];
+
 function totalNumberOfFibonacci(maximumCount: number) {
     let size = maximumCount;
     if (size > 0) {
-        console.log("the fibonacci sequence is: " + number1);
-        sum += number1;
+        arrFibonacci.push(number1);
         nextNumber = number1 + number2;
         number1 = number2;
         number2 = nextNumber;
-            size = size - 1;
-            totalNumberOfFibonacci(size);
-    }else {
-            console.log("total is: "+sum);
-        }
+        size = size - 1;
+        totalNumberOfFibonacci(size);
+    }
+    return arrFibonacci;
 }
 
- totalNumberOfFibonacci(20);
+function sumFibonacciSequence(array: Array<number>) {
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
+};
+arrFibonacci = totalNumberOfFibonacci(20);
+console.log(arrFibonacci);
+sum = sumFibonacciSequence(arrFibonacci);
+console.log(sum);
